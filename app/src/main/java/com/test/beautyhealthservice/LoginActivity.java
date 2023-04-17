374829a9dddf5c29cb672537c72290d47e11a4a4
shahili123 <shahili.shaik21@gmail.com>
2023-04-18 04:33:42 +0500

    Login registration page design commit with firebase library integration

---
+++ app/src/main/java/com/test/beautyhealthservice/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.test.beautyhealthservice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
