80500a2b8c4a6db306db3d7a7968cc2e3ebbb0fc
shahili123 <shahili.shaik21@gmail.com>
2023-04-27 13:54:15 +0500

    commit for user side fragment classes

---
+++ app/src/main/java/com/test/beautyhealthservice/Fragments/UserFragments/fragment_user_messages.java
@@ -0,0 +1,52 @@
+package com.test.beautyhealthservice.Fragments.UserFragments;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.test.beautyhealthservice.R;
+
+public class fragment_user_messages extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public fragment_user_messages() {
+    }
+
+    public static fragment_user_messages newInstance(String param1, String param2) {
+        fragment_user_messages fragment = new fragment_user_messages();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_user_messages, container, false);
+
+        return root;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+
+}
+
+
